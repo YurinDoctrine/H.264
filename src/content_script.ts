2536d343c2737a0d099fc71dfc49c50736c0efa4
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2020-07-28 13:09:22 +0300

    Add files via upload

---
+++ src/content_script.ts
@@ -0,0 +1,11 @@
+// Set defaults for options stored in localStorage
+chrome.storage.local.get({ enable: true, block_60fps: true }, (options) => {
+	const optionsJson = JSON.stringify(options);
+
+	const injectScript = document.createElement('script');
+	injectScript.textContent = `${inject.toString()}; inject(${optionsJson});`; // Use textContent instead of src so it's synchronous
+	injectScript.onload = () => injectScript.parentNode.removeChild(injectScript); // Remove <script> node after it has run
+
+	const injectParent = document.head || document.documentElement;
+	injectParent.appendChild(injectScript);
+});
