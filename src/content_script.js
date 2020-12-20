35b459d8a141b6744f866af4214f710f378b2f7e
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2020-12-20 16:32:19 +0300

    Add files via upload

--- src/content_script.js
+++ src/content_script.js
@@ -1,15 +1,13 @@
-// Set defaults for options stored in localStorage
 newFunction();
 function newFunction() {
-  chrome.storage.local.get({ enable: true }, (options) => {
-    const optionsJson = JSON.stringify(options);
+	chrome.storage.local.get({ enable: true }, (options) => {
+		const optionsJson = JSON.stringify(options);
 
-    const injectScript = document.createElement("script");
-    injectScript.textContent = `${inject.toString()}; inject(${optionsJson});`; // Use textContent instead of src so it's synchronous
-    injectScript.onload = () =>
-      injectScript.parentNode.removeChild(injectScript); // Remove <script> node after it has run
+		const injectScript = document.createElement('script');
+		injectScript.textContent = `${inject.toString()}; inject(${optionsJson});`; // Use textContent instead of src so it's synchronous
+		injectScript.onload = () => injectScript.parentNode.removeChild(injectScript); // Remove <script> node after it has run
 
-    const injectParent = document.head || document.documentElement;
-    injectParent.appendChild(injectScript);
-  });
+		const injectParent = document.head || document.documentElement;
+		injectParent.appendChild(injectScript);
+	});
 }
--- src/inject.js
+++ src/inject.js
@@ -1,36 +1,37 @@
+function save_options() {
+	const enable = document.getElementById('enable').checked;
+	chrome.storage.local.set({
+		enable: enable
+	});
+}
+
 function inject(options) {
-  if (!options.enable) return;
+	if (!options.enable) return;
 
-  override();
+	override();
 
-  function override() {
-    // Override video element canPlayType() function
-    const videoElem = document.createElement("video");
-    const origCanPlayType = videoElem.canPlayType.bind(videoElem);
-    videoElem.__proto__.canPlayType = makeModifiedTypeChecker(origCanPlayType);
+	function override() {
+		const videoElem = document.createElement('video');
+		const origCanPlayType = videoElem.canPlayType.bind(videoElem);
+		videoElem.__proto__.canPlayType = makeModifiedTypeChecker(origCanPlayType);
 
-    // Override media source extension isTypeSupported() function
-    if ((mse = window.MediaSource)) {
-      const origIsTypeSupported = mse.isTypeSupported.bind(mse);
-      mse.isTypeSupported = makeModifiedTypeChecker(origIsTypeSupported);
-    }
-  }
+		if ((mse = window.MediaSource)) {
+			const origIsTypeSupported = mse.isTypeSupported.bind(mse);
+			mse.isTypeSupported = makeModifiedTypeChecker(origIsTypeSupported);
+		}
+	}
 
-  // return a custom MIME type checker that can defer to the original function
-  function makeModifiedTypeChecker(origChecker) {
-    // Check if a video type is allowed
-    return function (type) {
-      if (type === undefined) return "";
+	function makeModifiedTypeChecker(origChecker) {
+		return function(type) {
+			if (type === undefined) return '';
 
-      const disallowed_types = ["av01", "vp9", "webm"];
+			const disallowed_types = [ 'av01', 'vp9', 'vp8', 'webm' ];
 
-      // If video type is in disallowed_types, say we don't support them
-      for (var i = 0; i < disallowed_types.length; i++) {
-        if (type.indexOf(disallowed_types[i]) !== -1) return "";
-      }
+			for (var i = 0; i < disallowed_types.length; i++) {
+				if (type.indexOf(disallowed_types[i]) !== -1) return '';
+			}
 
-      // Otherwise, ask the browser
-      return origChecker(type);
-    };
-  }
+			return origChecker(type);
+		};
+	}
 }
